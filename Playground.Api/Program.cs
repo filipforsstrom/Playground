cbe6ae0075425cf51445b49bcd803df23c3c7813
filipforsstrom <filipforsstrom@gmail.com>
2023-05-12 21:06:12 +0200

    get getId and post

--- Playground.Api/Program.cs
+++ Playground.Api/Program.cs
@@ -1,8 +1,15 @@
+using Playground.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/todoitems", () => "All todos");
 
-app.MapGet("todos", () => "Get all todos");
+app.MapGet("/todoitems/{id}", (int id) => "A todo with id " + id);
+
+app.MapPost("/todoitems", (Todo todo) =>
+{
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 app.Run();
