b567dbac570862a73d151b9624e420220890d0d5
filipforsstrom <filipforsstrom@gmail.com>
2023-05-15 15:53:50 +0200

    Todo with mongo annotations

--- Playground.Api/Models/Todo.cs
+++ Playground.Api/Models/Todo.cs
@@ -1,7 +1,12 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
 namespace Playground.Api.Models;
 
 public class Todo
 {
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsComplete { get; set; }
